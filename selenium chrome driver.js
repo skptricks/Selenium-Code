e96cefcfdd30f480b8713b1deff65789c97bba77
sumitking <sumit.pradhan96@gmail.com>
2017-06-24 16:23:28 +0530

    Update selenium chrome driver.js

--- selenium chrome driver.js
+++ selenium chrome driver.js
@@ -50,8 +50,34 @@ myWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='rs
         // exit the program explicitly
         System.exit(0);
 
-//
+// Close the browser window
+#######################################
+driver.quit();  // using QUIT all windows will close
+driver.close(); // close current window
+
+// Extract tag name
 ########################################################
-	    
+tagName = driver.findElement(By.id("email")).getTagName();
+
+output:
+------------
+input  <tag>
+	
+// Locating elements the element of page	
+################################################################
+Summary for locating elements
+
+Variation	 Description	                                                         Sample
+----------       ------------------------------------------------                        ---------------
+By.className	finds elements based on the value of the "class" attribute	        findElement(By.className("someClassName"))
+By.cssSelector	finds elements based on the driver's underlying CSS Selector engine	findElement(By.cssSelector("input#email"))
+By.id	locates elements by the value of their "id" attribute	                        findElement(By.id("someId"))  
+By.linkText	finds a link element by the exact text it displays	                findElement(By.linkText("REGISTRATION"))  
+By.name	locates elements by the value of the "name" attribute	                        findElement(By.name("someName"))  
+By.partialLinkText	locates elements that contain the given link text	        findElement(By.partialLinkText("REG"))  
+By.tagName	locates elements by their tag name	                                findElement(By.tagName("div"))  
+By.xpath	locates elements via XPath	                                        findElement(By.xpath("//html/body/div/tr[5]"))
+
+	
 
 
