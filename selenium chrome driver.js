b7ea28973b35797abaefdbd615abdae3dc4a72af
sumitking <sumit.pradhan96@gmail.com>
2017-06-24 15:53:29 +0530

    Update and rename selenium chrome driver.java to selenium chrome driver.js

---
+++ selenium chrome driver.js
@@ -0,0 +1,9 @@
+// load the webpage using chrome driver...
+
+System.out.println("launching chrome browser");
+System.setProperty("webdriver.chrome.driver", "D:\\selenium_sumit\\chromedriver_win32\\chromedriver.exe");
+driver = new ChromeDriver();	    
+//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+driver.navigate().to("https://www.google.com/");	    
+driver.manage().window().maximize();  // maximize window
+
