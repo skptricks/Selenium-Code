389b3e274e151f40f9c0d6fcb42764d8370a0ddd
sumitking <sumit.pradhan96@gmail.com>
2017-06-24 16:01:07 +0530

    Update selenium chrome driver.js

--- selenium chrome driver.js
+++ selenium chrome driver.js
@@ -1,9 +1,27 @@
+.......................................
+Source code of selenium Web Driver
+........................................
+
+
 // load the webpage using chrome driver...
+###############################################################
 
 System.out.println("launching chrome browser");
 System.setProperty("webdriver.chrome.driver", "D:\\selenium_sumit\\chromedriver_win32\\chromedriver.exe");
 driver = new ChromeDriver();	    
-//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-driver.navigate().to("https://www.google.com/");	    
+driver.navigate().to("https://www.google.com/");
+//driver.get("https://www.google.com/");
+
+// maximize the browser window...
+##########################################
+
 driver.manage().window().maximize();  // maximize window
+
+
+// Web driver implicit and explict wait...
+######################################################
+
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    
+
 
